d62c70b41082ee01ae5b065763541a74c4eaa00d
Jeremy Gonzalez <jeremy.gonzalez1128@gmail.com>
2022-08-15 17:11:56 -0400

    delete unused dao

--- src/main/java/com/example/studentrecords/data/StudentRecordsDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.studentrecords.data;
-
-import com.example.studentrecords.model.Student;
-
-public interface StudentRecordsDao {
-
-    public Student createStudent(String first_name, String last_name);
-}
